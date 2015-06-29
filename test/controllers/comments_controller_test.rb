04f6abc86c5761cce2e0257e9836585b6c6db97d
Joe Frey <jjfrey@gmail.com>
2015-06-29 17:24:57 -0400

    adding replies test to index

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -21,4 +21,16 @@ class CommentsControllerTest < ActionController::TestCase
 
   end
 
+  test "should get index with replies for comments" do
+    reply = Comment.create(author: 'one reply', text: 'I second that', parent_id: @comment.id)
+    get :index
+    assert_response :success
+    presenter = assigns(:presenter)
+    first_comment = presenter[:comments][0]
+    first_reply = first_comment[:replies][0]
+
+    assert_equal(reply, first_reply)
+  end
+
+
 end
