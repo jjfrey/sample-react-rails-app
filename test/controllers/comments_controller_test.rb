e3150814d4d603e5c8a23f435d52668fef824ffd
Joe Frey <jjfrey@gmail.com>
2015-06-29 15:56:54 -0400

    fixing Guardfile to detect *_test.rb files

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
+
+  def setup
+    @comment = comments(:one)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:presenter)
   end
+
+  test "should post create" do
+    post :create, (@comment) 
+    assert_not_nil Comment.where(:author => @comment.author)
+  end
+
 end
